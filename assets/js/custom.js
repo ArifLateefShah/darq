96ee79e0cd7b13e538a00feda089aec5a4e418c5
Murugan M <murugan.m@ideas2it.com>
2021-05-18 17:05:20 +0530

    Updated client feedbacks

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -65,7 +65,7 @@ $(function() {
                 $this.prop('disabled', true);
                 var form_data = $("#newsletter-form").serialize();
                 $.ajax({
-                    url:base_url+"/darq/php/newsletter.php",
+                    url:base_url+"/php/newsletter.php",
                     method:"POST",
                     data:form_data,
                     success:function(){
